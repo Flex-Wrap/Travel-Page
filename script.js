6a175b711392f228319611efbd480366213c3c45
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2024-09-10 09:56:04 +0200

    more css for fonts

--- script.js
+++ script.js
@@ -7,4 +7,25 @@ document.addEventListener('DOMContentLoaded', function() {
             this.classList.add('active');
         });
     });
+
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            // Get the target section based on the link's href attribute
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            // Get the target element's position offset by 50px
+            const offsetPosition = targetElement.getBoundingClientRect().top + window.scrollY - 50;
+
+            // Smoothly scroll to the calculated position
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
 });
